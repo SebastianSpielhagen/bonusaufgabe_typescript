d30d63b718c890234013ebc5ccf076bd07d78705
Sebastian Spielhagen <s.spielhagen@me.com>
2023-12-05 11:29:37 +0100

    add bonus 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,16 @@ import './App.css'
 
 function App() {
     class Schueler {
+        noten: { [fach: string]: (number | string | undefined)[] };
+
         constructor(
             public vorname: string,
             public nachname: string,
             public alter: number,
-            public noten: (number | string | undefined)[]
-        ) {}
+            noten: { [fach: string]: (number | string | undefined)[] }
+        ) {
+            this.noten = noten;
+        }
 
         noten_ausgeben(): void {
             const nameLength = this.vorname.length + this.nachname.length + this.alter.toString().length + 4;
@@ -16,7 +20,11 @@ function App() {
 
             console.log(`${this.vorname} ${this.nachname} (${this.alter})`);
             console.log(separator);
-            console.log("Grades: " + this.noten.map(n => (n === undefined ? "*" : n)).join(", "));
+
+            for (const fach in this.noten) {
+                console.log(`${fach}: ${this.noten[fach].map(n => (n === undefined ? "*" : n)).join(", ")}`);
+            }
+
             console.log();
         }
 
@@ -28,9 +36,21 @@ function App() {
     }
 
     const schuelerListe = [
-        new Schueler("Anton", "Meier", 16, [1, 4, 3, 1, "A", undefined, 1, 2]),
-        new Schueler("Berta", "Müller", 17, ["A", undefined, 1]),
-        new Schueler("Cäsar", "Schmidt", 17, ["A", 1, undefined, 3, 2, 4, 5])
+        new Schueler("Anton", "Meier", 16, {
+            Sport: [1, 4, 3, 1, "A", undefined, 1, 2],
+            Kunst: ["A", undefined, 1],
+            Mathe: ["A", 1, undefined, 3, 2, 4, 5]
+        }),
+        new Schueler("Berta", "Müller", 17, {
+            Sport: ["A", undefined, 1],
+            Kunst: [3, 2, 4, 5],
+            Mathe: [1, 2, "A"]
+        }),
+        new Schueler("Cäsar", "Schmidt", 17, {
+            Sport: ["A", 1, undefined, 3, 2, 4, 5],
+            Kunst: [3, 2, 4, 5],
+            Mathe: [1, 2, "A"]
+        })
     ];
 
     Schueler.schueler_liste_ausgeben(schuelerListe);
